00848db3521358d9d162557d7f45c5e7892370f5
tyr0wl <paul.ch87@gmail.com>
2021-05-10 22:22:48 +0200

    Refactor ModInfo into seperate file

--- DSP_AssemblerUI/AssemblerSpeedUI/AssemblerSpeedUIMod.cs
+++ DSP_AssemblerUI/AssemblerSpeedUI/AssemblerSpeedUIMod.cs
@@ -18,17 +18,6 @@ using UnityEngine.UI;
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 namespace DSP_AssemblerUI.AssemblerSpeedUI
 {
-    public static class ModInfo
-    {
-        public const string ModID = "dsp.assemblerUI.speedMod";
-        public const string ModName = "Assembler UI Speed Info Mod";
-        public const string majorVersion = "1";
-        public const string minorVersion = "2";
-        public const string hotfixVersion = "0";
-        public const string buildVersion = "0";
-        public const string VersionString = majorVersion + "." + minorVersion + "." + hotfixVersion + "." + buildVersion;
-    }
-
     [BepInPlugin(ModInfo.ModID, ModInfo.ModName, ModInfo.VersionString)]
     public class AssemblerSpeedUIMod : BaseUnityPlugin
     {
