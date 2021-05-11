5b4aa50f4e858bfa45247c7a8c821b6ca84a1660
tyr0wl <paul.ch87@gmail.com>
2021-05-11 02:15:35 +0200

    Add Constants class with speed text path

---
+++ DSP_AssemblerUI/AssemblerSpeedUI/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace DSP_AssemblerUI.AssemblerSpeedUI
+{
+    internal static class Constants
+    {
+        public static readonly string BaseWindowsPath = "UI Root/Overlay Canvas/In Game/Windows";
+        public static readonly string AssemblerWindowPath = $"{BaseWindowsPath}/Assembler Window";
+        public static readonly string AssemblerWindowSpeedTextPath = $"{AssemblerWindowPath}/produce/speed/speed-text";
+    }
+}
